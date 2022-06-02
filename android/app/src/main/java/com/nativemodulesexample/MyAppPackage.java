6dd725f12b31f073ade9e6afcc714497899ce106
Your Name <you@example.com>
2022-06-02 19:19:57 -0400

    Ejemplo de modulos nativos

---
+++ android/app/src/main/java/com/nativemodulesexample/MyAppPackage.java
@@ -0,0 +1,28 @@
+package com.nativemodulesexample;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MyAppPackage implements ReactPackage {
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+        final List<NativeModule> modules = new ArrayList<NativeModule>();
+        modules.add(new CalendarModule(reactContext));
+        return modules;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
